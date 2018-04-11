c804e88dc2f128879c186da13e3946cd25dd0e16
waiiinta <waiiboy@hotmail.com>
2018-04-11 12:59:28 +0700

    remove comment in SplitFile.py

--- SplitFile.py
+++ SplitFile.py
@@ -1,16 +1,3 @@
-# import xml.etree.ElementTree as ET
-# tree = ET.parse('example.xml')
-# root = tree.getroot()
-# Header = ["PMID","PubDate","Title","ArticleTitle",]
-# parent_map = dict((c, p) for p in tree.getiterator() for c in p)
-
-# for child in tree.getiterator():
-#     #if(child.tag is not in )
-#     if(child.tag not in Header):
-#         print("remove")
-# for r in parent_map:
-#     parent_map.remove(r)
-
 import xml.etree.ElementTree as ET
 import os
 #file = xml file input(.xml)
